728bc84d7db02578470567af914b4b4cfb7c4e30
Mahdi Karimi <mhdi_kr@outlook.com>
2020-11-27 11:57:57 +0330

    writing scripts and handling headers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-const cp = require('child_process')
 const express = require('express')
+const parser = ('body-parser')
+const cp = require('child_process')
+
 const app = express()
+app.use(parser.json())
+app.use(parser.urlencoded({
+  extended: true
+}))
 const port = 3030
 app.post('/update', (req, res) => {
     console.log(`Reciving POST request from: placeholders`);
@@ -8,11 +14,13 @@ app.post('/update', (req, res) => {
     console.log(req.header,req.body);
     // run needed commands here
     console.log('Running the scripts sequences:');
-    cp.exec('echo cpworking!',(e,stdout,stdrr)=>{
+    cp.execSync('cd ../../emergency-storage',(e,stdout,stdrr)=>{
+      console.log(stdout);
+    })
+    cp.execSync('git pull',(e,stdout,stdrr)=>{
       console.log(stdout);
     })
     res.sendStatus(200)
-    res.send('got it')
     // cp.execSync('cd ../')  
     // cp.execSync('cd backend && git pull')
 })
