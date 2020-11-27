14471e1cbd06bc1557be40bf196d03541cd2a707
Mahdi Karimi <mhdi_kr@outlook.com>
2020-11-27 18:06:07 +0330

    shell file with child

---
+++ src/deploy.sh
@@ -0,0 +1,13 @@
+cd ../../emergency-storage/
+git pull
+(
+    cd Backend
+    npm install
+    npm start
+)
+(
+    cd Frontend
+    npm install
+    npm run build
+    npm start
+)
--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
+require('dotenv').config();
 const express = require('express')
 const parser = require('body-parser')
 const cp = require('child_process')
-
 const app = express()
+
 app.use(parser.json())
 app.use(parser.urlencoded({
   extended: true
 }))
 
-require('dotenv').config();
-console.log(process.env.REPO_UUID);
-
 const port = 3030
+
 app.post('/update', (req, res) => {
   if(req.body.repository.uuid === process.env.REPO_UUID){
     console.log(`Reciving POST request from bitbucket, by ${req.body.push.actor.display_name}`);
     // run needed commands here
-    console.log('Running the scripts sequences:');
-    cp.execSync('./../deploy.sh',(e,stdout,stdrr)=>{
-      console.log(stdout);
+    console.log('Running deployment shell script:');
+    const deploySequence = cp.execFile('deploy.sh', (e,stdout,stdrr) => {
+      console.log(e,stdout,stderr);
     })
     res.sendStatus(200)
   }
