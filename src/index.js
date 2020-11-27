3e7aa8bc84cc836c5ff086e7b7dc8fff430318ad
Mahdi Karimi <mhdi_kr@outlook.com>
2020-11-27 11:38:07 +0330

    figuring out the header and body structure

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const port = 3030
 app.post('/update', (req, res) => {
     console.log(`Reciving POST request from: placeholders`);
     console.log('details:');
-    console.log(req.body,res.body);
+    console.log(req.header,req.body);
     // run needed commands here
     console.log('Running the scripts sequences:');
     cp.exec('echo cpworking!',(e,stdout,stdrr)=>{
       console.log(stdout);
     })
-    // cp.execSync('cd ../')
+    res.sendStatus(200)
+    res.send('got it')
+    // cp.execSync('cd ../')  
     // cp.execSync('cd backend && git pull')
 })
 app.listen(port, () => {
