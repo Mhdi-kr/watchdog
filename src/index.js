0a7f9bb26ec24744284cd21b5e24bd3500f7e2da
Mahdi Karimi <mhdi_kr@outlook.com>
2020-11-27 11:33:35 +0330

    verifying and handling post request

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ const app = express()
 const port = 3030
 app.post('/update', (req, res) => {
     console.log(`Reciving POST request from: placeholders`);
+    console.log('details:');
+    console.log(req.body,res.body);
     // run needed commands here
-    // cp.execSync('cd frontend && git pull')
+    console.log('Running the scripts sequences:');
+    cp.exec('echo cpworking!',(e,stdout,stdrr)=>{
+      console.log(stdout);
+    })
     // cp.execSync('cd ../')
     // cp.execSync('cd backend && git pull')
 })
